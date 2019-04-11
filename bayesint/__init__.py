242630631467913d0a8151e12ffd5c7622e9a84d
Thomas Finnie <Thomas.Finnie@phe.gov.uk>
2019-04-11 13:19:46 +0100

    Update versioning system

--- bayesint/__init__.py
+++ bayesint/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import absolute_import, division, print_function
-from .version import __version__
 
 from .table_measures import *
 from .table_tests import *
 from .random_variables import *
 from .intervals import *
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = 'unknown'
--- setup.py
+++ setup.py
@@ -2,15 +2,9 @@ import os
 import io
 import unittest
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 root = os.path.dirname(__file__)
-
-with open(os.path.join(root, 'bayesint', 'version.py')) as f:
-    exec(f.read())
 
 with io.open(os.path.join(root, 'README.md'), encoding='utf8') as f:
     readme = f.read()
@@ -18,14 +12,18 @@ with io.open(os.path.join(root, 'README.md'), encoding='utf8') as f:
 with io.open(os.path.join(root, 'LICENSE'), encoding='utf8') as f:
     license = f.read()
 
+setup_requires=[
+    'setuptools-scm>=3.2.0',
+    ]
+
 setup(name='bayesint',
-      version=__version__,
+      use_scm_version=True,
       python_requires='>=2.7.14, !=3.0.*, !=3.1.*, !=3.3.*, !=3.4.*, >=3.5.5, >=3.6.6',
       packages=['bayesint'],
       description='Bayesian credible intervals for ratios',
       long_description=readme,
       author='Public Health England',
-      author_email='maria.dunbar@phe.gov.uk',
+      author_email='thomas.finnie@phe.gov.uk',
       maintainer='Public Health England',
       license=license,
       classifiers=['Programming Language :: Python',
@@ -38,5 +36,6 @@ setup(name='bayesint',
           'scipy>=0.19.1',
           'sympy>=1.1.1',
           'numpy>=1.13.3'],
-      test_suite='tests.test_suite_loader'
+      test_suite='tests.test_suite_loader',
+      setup_requires=setup_requires,
 )
